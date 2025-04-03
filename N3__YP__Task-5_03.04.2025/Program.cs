7afc96b2b5eee2bc5e64d7d6494bd5259713db31
Loli Admin <Loli Admin@DESKTOP-H4SR9KC>
2025-04-03 22:55:33 +0300

    Добавьте файлы проекта.

---
+++ N3__YP__Task-5_03.04.2025/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace N3__YP__Task_5_03._04._2025
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("N: ");
+            int n = int.Parse(Console.ReadLine());
+            int s = 1, t = 1, d = -1;
+            for (int i = 2; i <= n; i++)
+            {
+                s = 2 * s + i;
+                t += s;
+                d = s > 100 ? i : d;
+            }
+            Console.WriteLine($"T({n}) = {t}");
+            Console.WriteLine(d > 0 ? $"S > 100 на {d}-й день" : "S <= 100 всегда");
+        }
+    }
+}
